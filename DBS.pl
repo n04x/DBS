f5771d5089755367c87d59834b4e1dfc76aadce9
n04x <ThomasBacks@gmail.com>
2017-05-23 12:58:19 -0400

    Update DBS.pl with Kaioshin
    Add a list of kaioshin facts in the knowledge base, kaioshin(name, universe).

--- DBS.pl
+++ DBS.pl
@@ -12,7 +12,7 @@ universe(10).
 universe(11).
 universe(12).
 
-%% Ranking; go like this rank(universe, position).
+%% Rank sorted by their universe; rank(universe, position).
 rank(1, 1).
 rank(2, 6).
 rank(3, 7).
@@ -25,3 +25,17 @@ rank(9, 12).
 rank(10, 8).
 rank(11, 5).
 rank(12, 2).
+
+%% List of kaioshin with their respective universe; kaioshin(name, universe).
+kaioshin(anat, 1).
+kaioshin(peru, 2).
+kaioshin(ea, 3).
+kaioshin(kuru, 4).
+kaioshin(ogma, 5).
+kaioshin(fuwa, 6).
+kaioshin(shin, 7).
+kaioshin(iru, 8).
+kaioshin(roh, 9).
+kaioshin(gowasu, 10).
+kaioshin(khai, 11).
+kaioshin(ugg, 12).
