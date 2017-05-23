e76210e55a430d5aeaf5785ec82d216c5847a228
n04x <ThomasBacks@gmail.com>
2017-05-23 12:52:52 -0400

    Add rank
    add ranking in the knowledge base the code is rank(unvierse, position).

--- DBS.pl
+++ DBS.pl
@@ -11,3 +11,17 @@ universe(9).
 universe(10).
 universe(11).
 universe(12).
+
+%% Ranking; go like this rank(universe, position).
+rank(1, 1).
+rank(2, 6).
+rank(3, 7).
+rank(4, 9).
+rank(5, 3).
+rank(6, 10).
+rank(7, 11).
+rank(8, 4).
+rank(9, 12).
+rank(10, 8).
+rank(11, 5).
+rank(12, 2).
